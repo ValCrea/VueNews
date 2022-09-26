e9454fc767efedfe6ff7dd38683a0db567dd9b99
ValCrea <val.waldner@creatim.com>
2022-09-26 11:06:45 +0200

    Debounce search

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <script setup lang="ts">
 import { ref, computed, watch } from "vue";
+import debounce from "lodash/debounce";
 import { useNewsStatic } from "@/api/news";
 import NewsArticle from "./components/NewsArticle.vue";
 
 const searchQuery = ref("");
-const enabled = computed(() => searchQuery.value != "");
-const { isLoading, isError, isIdle, data, error } = useNewsStatic(searchQuery, {
-  enabled,
-});
+const enabled = ref(false);
+const { isFetching, isError, isIdle, data, error, refetch } = useNewsStatic(
+  searchQuery,
+  {
+    enabled,
+  }
+);
 
-watch(isLoading, () => {
-  console.log("LOADING");
+watch(searchQuery, () => {
+  enabled.value = !!searchQuery.value;
+  if (searchQuery.value) debounce(refetch.value, 1000)();
+  else debounce(refetch.value, 1000).cancel();
 });
 </script>
 
 <template>
   <div>
-    {{ isIdle }}
+    {{ enabled }}
     <header class="header">
       <input v-model="searchQuery" class="header__search" type="text" />
     </header>
     <main class="news">
       <p v-if="isIdle">Search for article</p>
-      <p v-else-if="isLoading" class="news__loading">Loading...</p>
+      <p v-else-if="isFetching" class="news__loading">Loading...</p>
       <p v-else-if="isError" class="news__error">{{ error }}</p>
       <div v-else class="news__articles">
         <p class="news__count">Total resoults: {{ data.totalResults }}</p>
